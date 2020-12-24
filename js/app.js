92f3837d297b2ca7fa2e37f5b8019449d208fa6f
Ethan Ma <em682@cornell.edu>
2020-12-23 20:40:44 -0500

    doen for today

--- js/app.js
+++ js/app.js
@@ -5,7 +5,8 @@ var container,
 	renderer,
 	plane,
     mouseMesh,
-    saber;
+    saber,
+    road;
 
 // Custom global variables
 var mouse = {x: 0, y: 0};
@@ -17,7 +18,7 @@ function init() {
 
 	// Scene
     scene = new THREE.Scene();
-    
+    scene.position.set(0,0,0)
 
 	// Camera
 	var screenWidth = window.innerWidth,
@@ -41,7 +42,7 @@ function init() {
 
 	// Define the lights for the scene
 	var light = new THREE.PointLight(0xffffff);
-	light.position.set(20, 0, 20);
+	light.position.set(0, 0, 0);
 	scene.add(light);
 	var lightAmb = new THREE.AmbientLight(0x777777);
 	scene.add(lightAmb);
@@ -70,7 +71,7 @@ function init() {
     //});
     saber = new THREE.Mesh(bladeGeo, materialArray);
     saber.position.z = 0;
-    saber.rotation.x = -Math.PI / 4;
+    saber.rotation.x = -Math.PI / 6;
     scene.add(saber);
 	//mouseMesh = new THREE.Mesh(mouseGeometry, mouseMaterial);
     
@@ -100,15 +101,315 @@ function onMouseMove(event) {
 //	mouseMesh.position.set(event.clientX, event.clientY, 0);
 };
 
+
+const options = {
+    length: 400,
+    width: 20,
+    roadWidth: 9,
+    islandWidth: 2,
+    nPairs: 50,
+    roadSections: 3
+  };
+
+  const geometry = new THREE.PlaneBufferGeometry(
+    options.width,
+    options.length,
+    20,
+    200
+  );
+
+
+  const fragmentShader = `
+    uniform vec3 uColor;
+	void main(){
+        gl_FragColor = vec4(uColor,1.);
+    }
+    `;
+    const vertexShader = `
+    uniform float uTravelLength;
+    #include <getDistortion_vertex>
+        void main(){
+            vec3 transformed = position.xyz;
+            
+        float progress = (transformed.y + uTravelLength / 2.) / uTravelLength;
+        vec3 distortion  = getDistortion(progress);
+        transformed.x += distortion.x;
+        transformed.z += distortion.y;
+        gl_Position = projectionMatrix * modelViewMatrix * vec4(transformed.xyz, 1.);
+        }
+    `;
+    const lfragmentShader = `
+uniform vec3 uColor;
+  void main() {
+      vec3 color = vec3(uColor);
+      gl_FragColor = vec4(color,1.);
+  }
+`;
+
+const lvertexShader = `
+attribute vec3 aOffset;
+attribute vec2 aMetrics;
+uniform float uTime;
+uniform float uSpeed;
+uniform float uTravelLength;
+#include <getDistortion_vertex>
+  void main() {
+    vec3 transformed = position.xyz;
+    
+    float radius = aMetrics.r;
+    float len = aMetrics.g;
+    transformed.xy *= radius; 
+    transformed.z *= len;
+
+    float zOffset = uTime * uSpeed + aOffset.z;
+    zOffset = len - mod(zOffset, uTravelLength);
+
+    // transformed.z +=uTime * uSpeed;
+
+
+		// Keep them separated to make the next step easier!
+	   transformed.z = transformed.z +zOffset ;
+        transformed.xy += aOffset.xy;
+
+        
+    float progress = abs(transformed.z / uTravelLength);
+    transformed.xyz += getDistortion(progress);
+
+	
+        vec4 mvPosition = modelViewMatrix * vec4(transformed,1.);
+        gl_Position = projectionMatrix * mvPosition;
+	}
+`;
+
+const distortion_uniforms = {
+    uDistortionX: new THREE.Uniform(new THREE.Vector2(40, 3)),
+    uDistortionY: new THREE.Uniform(new THREE.Vector2(-10, 2.5))
+  };
+  
+const distortion_vertex = `
+#define PI 3.14159265358979
+uniform vec2 uDistortionX;
+uniform vec2 uDistortionY;
+
+    float nsin(float val){
+    return sin(val) * 0.5+0.5;
+    }
+vec3 getDistortion(float progress){
+        progress = clamp(progress, 0.,1.);
+        float xAmp = uDistortionX.r;
+        float xFreq = uDistortionX.g;
+        float yAmp = uDistortionY.r;
+        float yFreq = uDistortionY.g;
+        return vec3( 
+            xAmp * nsin(progress* PI * xFreq   - PI / 2. ) ,
+            yAmp * nsin(progress * PI *yFreq - PI / 2.  ) ,
+            0.
+        );
+    }
+`;
+
+const myCustomDistortion = {
+    uniforms: distortion_uniforms,
+    getDistortion: distortion_vertex,
+}
+
+const material = new THREE.ShaderMaterial({
+    opacity: 0.5,
+    transparent: true,
+fragmentShader:fragmentShader,
+vertexShader:vertexShader,
+uniforms: Object.assign(
+    {
+      uColor: new THREE.Uniform(new THREE.Color(0x000000)),
+      uTime: new THREE.Uniform(0),
+      uTravelLength: new THREE.Uniform(options.length)
+    },
+    myCustomDistortion.uniforms
+  )
+});
+
+material.onBeforeCompile = shader => {
+    shader.vertexShader = shader.vertexShader.replace(
+      "#include <getDistortion_vertex>",
+      myCustomDistortion.getDistortion
+    );
+  };
+
+road = new THREE.Mesh(geometry, material);
+
+road.rotation.x = -Math.PI/2;
+road.position.z = -options.length / 2;
+road.position.y =-5;
+scene.add(road);
+
+
+let curve = new THREE.LineCurve3(
+    new THREE.Vector3(0, 0, 0),
+    new THREE.Vector3(0, 0, -1)
+);
+let baseGeometry = new THREE.TubeBufferGeometry(curve, 50, 1, 8, false);
+let instanced = new THREE.InstancedBufferGeometry().copy(baseGeometry);
+instanced.instanceCount = options.nPairs * 2;
+
+let aOffset = [];
+let aMetrics = [];
+
+let sectionWidth = options.roadWidth / options.roadSections;
+
+for (let i = 0; i < options.nPairs; i++) {
+    let radius = Math.random() * 0.1 + 0.1;
+    let length =
+        Math.random() * options.length * 0.08 + options.length * 0.02;
+    // 1a. Get it's lane index
+    // Instead of random, keep lights per lane consistent
+    let section = i % 3;
+
+    // 1b. Get its lane's centered position
+    let sectionX =
+    section * sectionWidth - options.roadWidth / 2 + sectionWidth / 2;
+    let carWidth = 0.5 * sectionWidth;
+    let offsetX = 0.5 * Math.random();
+
+    let offsetY = radius * 1.3;
+
+    let offsetZ = Math.random() * options.length;
+
+    aOffset.push(sectionX - carWidth / 2 + offsetX);
+    aOffset.push(offsetY);
+    aOffset.push(-offsetZ);
+
+    aOffset.push(sectionX + carWidth / 2 + offsetX);
+    aOffset.push(offsetY);
+    aOffset.push(-offsetZ);
+
+    aMetrics.push(radius);
+    aMetrics.push(length);
+
+    aMetrics.push(radius);
+    aMetrics.push(length);
+}
+// Add the offset to the instanced geometry.
+instanced.setAttribute(
+    "aOffset",
+    new THREE.InstancedBufferAttribute(new Float32Array(aOffset), 3, false)
+);
+instanced.setAttribute(
+    "aMetrics",
+    new THREE.InstancedBufferAttribute(new Float32Array(aMetrics), 2, false)
+  );
+const rightLightMaterial = new THREE.ShaderMaterial({
+    fragmentShader:lfragmentShader,
+    vertexShader:lvertexShader,
+    uniforms: Object.assign(
+        {
+          uColor: new THREE.Uniform(new THREE.Color(0x53C2C6)),
+          uTravelLength: new THREE.Uniform(options.length),
+          uTime: new THREE.Uniform(0),
+          uSpeed: new THREE.Uniform(-5)
+        },
+        myCustomDistortion.uniforms
+      )
+});
+
+const leftLightMaterial = new THREE.ShaderMaterial({
+    opacity: 0.5,
+    transparent: true,
+    fragmentShader:lfragmentShader,
+    vertexShader:lvertexShader,
+    uniforms: Object.assign(
+        {
+          uColor: new THREE.Uniform(new THREE.Color(0xFF5F73)),
+          uTravelLength: new THREE.Uniform(options.length),
+          uTime: new THREE.Uniform(0),
+          uSpeed: new THREE.Uniform(-5)
+        },
+        myCustomDistortion.uniforms
+      )
+});
+
+leftLightMaterial.onBeforeCompile = shader => {
+    shader.vertexShader = shader.vertexShader.replace(
+      "#include <getDistortion_vertex>",
+      myCustomDistortion.getDistortion
+    );
+  };
+rightLightMaterial.onBeforeCompile = shader => {
+shader.vertexShader = shader.vertexShader.replace(
+    "#include <getDistortion_vertex>",
+    myCustomDistortion.getDistortion
+);
+};
+
+
+
+let rightLights = new THREE.Mesh(instanced, rightLightMaterial);
+rightLights.frustumCulled = false;
+rightLights.position.y =-5.2;
+rightLights.position.x = options.roadWidth / 2 + options.islandWidth / 2
+let leftLights = new THREE.Mesh(instanced, leftLightMaterial);
+leftLights.frustumCulled = false;
+leftLights.position.y =-5.2;
+leftLights.position.x =-options.roadWidth / 2 - options.islandWidth / 2
+scene.add(rightLights);
+scene.add(leftLights);
+
+var time = 0;
+setInterval(function () {
+  time++;
+  rightLights.material.uniforms.uTime.value = time;
+leftLights.material.uniforms.uTime.value = time;
+}, 50);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//var composer = new THREE.EffectComposer(renderer);
+
+//var renderPass = new THREE.RenderPass(scene,camera);
+
+//composer.addPass(renderPass);
+
+//var pass1 = new THREE.GlitchPass(0);
+//composer.addPass(pass1);
+//pass1.renderToScreen = true;
+
+
 // Animate the elements
 function animate() {
     requestAnimationFrame(animate);
 		render();	
 }
-	
+//render();
 // Rendering function
 function render() {
-
+    //composer.render();
+    //requestAnimationFrame(animate);
 	// For rendering
 	renderer.autoClear = false;
 	renderer.clear();
